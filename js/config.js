f9242e7e190b7dfed036671123b14e879d4e8159
abdulrahim0966 <abok0965@gmail.com>
2025-05-18 12:44:35 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Google Apps Script Web Uygulama URL'inizi buraya yazın
-const API_URL = "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwflj2Xkz44KXcAydTq4iuVzX2IqrOv9dHcnEqI9_QHsF0nXf7v3Xa4Qt9BUh3hoqIC/exec";
